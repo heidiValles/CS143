8c5bcaffe4f9fdd4043a8de50fe1fb680075cb71
heidiValles <hshvalles@gmail.com>
2018-02-09 22:47:03 -0700

    added all files to commit

---
+++ GrammarApp.java
@@ -0,0 +1,204 @@
+ import java.io.File;
+ import java.net.HttpURLConnection;
+ import java.net.MalformedURLException;
+ import java.net.URL;
+ import java.io.*;
+ import java.util.*;
+
+/**
+ * GrammarApp contains a main program that prompts a user for the name of a
+ * grammar file and then gives the user the opportunity to generate random
+ * versions of various elements of the grammar.
+ *
+ * STUDENT TO DO:  Implement the loadRules() method
+ *                 Modify main() to handle bad file input (reprompt the user)
+ *
+ * @author: B. Goldner, Heidi Valles
+ * @version 02/13/2018
+ */
+
+public class GrammarApp {
+  
+  static boolean fileExists = false; //stores boolean state of client's file existence
+  static File checkFile; //stores instances of fileName to check if they exist
+  static String fileName; //fileName entered by user
+  
+  /**
+   * Retrieves all of the rules from a given file and stores them in an arrayList
+   * @return ArrayList of rules from file given by client
+   */
+  public  static List<String> loadRules(String fileName) {
+    Scanner s = new Scanner(fileName);
+    List<String> rulesList = new ArrayList<>();
+    while (s.hasNext()) {
+      rulesList.add(s.next());
+    }
+    return rulesList;
+  }
+  
+  /**
+   * Method found online at:  http://www.rgagnon.com/javadetails/java-0059.html to see
+   * if an URL exists
+   * @param URLName to be checked
+   * @return whether an url exists
+   */
+  public static boolean exists(String URLName){
+    try {
+      HttpURLConnection.setFollowRedirects(false);
+      // note : you may also need
+      //        HttpURLConnection.setInstanceFollowRedirects(false)
+      HttpURLConnection con =
+        (HttpURLConnection) new URL(URLName).openConnection();
+      con.setRequestMethod("HEAD");
+      return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+  
+  public static void main(String[] args) throws Exception {
+  
+    URL oracle = new URL(" http://facweb.northseattle.edu/bgoldner/CSC143/143HW/HW4-BNF/sentence.txt");
+    String sOracle = oracle.toString();
+    if (exists(sOracle)) {
+      BufferedReader in = new BufferedReader(
+        new InputStreamReader(oracle.openStream()));
+  
+      String inputLine;
+      while ((inputLine = in.readLine()) != null)
+        System.out.println(inputLine);
+      in.close();
+    }
+  }
+  /**
+  public static void main(String[] args) throws IOException {
+    Scanner keyboard = new Scanner(System.in);
+  
+    //ask for new user input if file is not found
+    do {
+      fileName = intro(keyboard);
+      //checks to see if file name given by client is a local file or a web address
+      if (fileName.contains("http:")){
+        System.out.println("Found URL. Checking for validity");
+        URL oracle = new URL(" http://facweb.northseattle.edu/bgoldner/CSC143/143HW/HW4-BNF/sentence.txt");
+        String sOracle = oracle.toString();
+        if (exists(sOracle)) {
+          BufferedReader in = new BufferedReader(
+          new InputStreamReader(oracle.openStream()));
+          String inputLine;
+          while ((inputLine = in.readLine()) != null)
+           System.out.println(inputLine);
+           in.close();
+          }
+          URL url = new URL(fileName);
+          checkFile = new File(url.getFile());
+      }
+      else {
+        checkFile = new File(fileName);
+      }
+      if (checkFile.exists()) {
+        fileExists = true;
+      } else {
+          System.out.println("File not found, please enter valid file name: ");
+        }
+      }
+    while (fileExists == false) ;
+    
+    // run through generating grammar if file is found
+    do {
+      System.out.println("File has been found");
+      List<String> grammar = loadRules(fileName);
+      // construct the grammar generator
+      GrammarGenerator gen =
+        new GrammarGenerator(grammar);
+      // interact with user to generate expressions from the loaded grammar
+      String target = getSymbol(keyboard, gen);
+      while (target.length() != 0) {
+        int count = getCount(keyboard);
+        String[] answers = gen.generate(target, count);
+        System.out.println("****************************************");
+        for (int i = 0; i < count; i++) {
+          System.out.println(answers[i]);
+        }
+        target = getSymbol(keyboard, gen);
+      }
+    } while (fileExists);
+  }
+  
+    /**
+     * Display introduction and prompt for grammar filename
+     * @return filename from user
+     */
+  
+  public static String intro(Scanner input) {
+    System.out.println("Welcome to the random elements generator.");
+    System.out.println();
+    
+    // open grammar file and read it in
+    System.out.print("What is the name of the grammar file? ");
+    return input.nextLine();
+  }
+  
+  /**
+   * Prompts user for symbol from GrammarGenerator
+   *
+   * @param in  source of input
+   * @param gg current GrammarGenerator of rules
+   * @return either retuns a valid symbol from GrammarGenerator, or string of length 0
+   * to indicate the user wants to quit
+   */
+  public static String getSymbol(Scanner in, GrammarGenerator gg) {
+    printPrompt(gg);
+    
+    System.out.print("What do you want generated (return to quit)? ");
+    String target = in.nextLine();
+    while (target.length() != 0 && !gg.contains(target)) {
+      System.out.println("Illegal symbol. Please try again\n");
+      System.out.print("What do you want generated (return to quit)? ");
+      target = in.nextLine();
+    }
+    return target;
+  }
+  
+  /**
+   * Prints the list of possible symbols from gen
+   *
+   * @param gen current set of rules
+   */
+  public static void printPrompt(GrammarGenerator gen) {
+    System.out.println();
+    System.out.println("Available symbols to generate are:");
+    System.out.println(gen.getSymbols());
+  }
+  
+  /**
+   * Asks use for number of examples to generate. Performs input validation
+   *
+   * @param in the source of user input
+   * @return the valid input >= 0
+   */
+  public static int getCount(Scanner in) {
+    int userCount = -1;  // flag value until have legitimate input
+  
+    while (userCount == -1) {
+      System.out.print("How many do you want me to generate? ");
+      if (!in.hasNextInt()) {
+        System.out.println("that's not an integer");
+        System.out.println();
+        in.nextLine();  // consume the garbage from the input buffer
+      } else {
+        // read in the number and see if it is legit
+        userCount = in.nextInt();
+        in.nextLine();  // consume whitespace left over from the integer input
+        if (userCount <= 0) {
+          System.out.println("must be positive input");
+          System.out.println();
+          userCount = -1;
+        }
+      }
+    }
+    return userCount;
+  }
+}
---
+++ GrammarGenerator.java
@@ -0,0 +1,49 @@
+import java.util.List;
+
+/**
+ * Supporting class for GrammarApp that will generate different grammars according to
+ * client needs.
+ * @author Heidi Valles
+ * @version 02/13/2018
+ */
+public class GrammarGenerator {
+  
+  /**
+   * Constructor is passed a grammar as a List of Strings and stores the List so as to
+   * later generate parts of the grammar
+   * @param Grammar in the file that client submits to program
+   * @throws IllegalArgumentException if there are 2+ entries in the grammar for the
+   * same non-terminal
+   */
+  public GrammarGenerator(List<String> Grammar){
+  
+  }
+  
+  /**
+   * Checks to see if the given symbol is a non-terminal of the grammar
+   * @param symbol given by client to check for non-terminal/terminal status
+   * @return whether symbol is non-terminal or not
+   */
+  public boolean contains(String symbol){
+    return true;
+  }
+  
+  /**
+   * Will randomly generate tho given number of occurrences of the given symbol.
+   * @param symbol which will be repeated by a client-specified amount of times
+   * @param times that the symbol will be repeated
+   * @return String that will contain the symbol x times
+   */
+  public String[] generate(String symbol, int times){
+    String[] generateSymbols = new String[times];
+    return generateSymbols;
+  }
+  
+  /**
+   * Gets the non-terminal symbols in a file and returns them as a string
+   * @return String representation of non-terminal symbols in a file
+   */
+  public String getSymbols(){
+  return "a";
+  }
+}
